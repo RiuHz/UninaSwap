e63c22f30efe8ea4d2e97a3c5d74d89993813e93
Gaetano Di Tota <ditotagaetano@outlook.it>
2025-06-08 15:44:03 +0200

    Prima implementazione del controlle per le funzioni basilari della GUI con valori di test per le funzioni

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -1,12 +1,38 @@
 package controller;
 
+import javax.swing.JFrame;
+import java.util.ArrayList;
 
+import entities.university.UniversityDAO;
+import entities.user.UserDAO;
 
 public class Controller {
+	private MainWindow loginFrame = new MainWindow(this);
+	// private appFrame = null;
+	private UserDAO user = new UserDAO();
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		loginFrame.show();
 	}
 
+	public void validateLogin(String username, String password) {
+		if (user.validLogin(username, password))
+			// appFrame.show();
+		else if (user.invalidUsername());
+			loginFrame.showInvalidUsernameError();
+		else
+			loginFrame.showInvalidPasswordError();
+	}
+	
+	public void validateRegistration(String username, String password, String name, String surname, String university) {
+		if (user.validRegistration(username, password, name, surname, university))
+			loginFrame.showSuccessRegistration();
+		else
+			loginFrame.showExistingUsernameError();
+	}
+	
+	public ArrayList<String> getUniversityList() {
+		return UniversityDAO.getNamesList();
+	}
+	
 }
---
+++ src/entities/university/UniversityDAO.java
@@ -0,0 +1,18 @@
+package entities.university;
+
+import java.util.ArrayList;
+
+public class UniversityDAO {
+	
+	public static ArrayList<String> getNamesList() {
+		
+		// TODO Fai la query per ottenere tutti i nomi di universitàle Università
+		
+		ArrayList<String> p = new ArrayList<String>();
+		
+		p.add("Università di Napoli Federico II");
+		
+
+		return p;
+	}
+}
